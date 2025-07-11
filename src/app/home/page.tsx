fcc28d0ad89151d2dde7246de8d64815f3917f78
정민규 <jmg2165@naver.com>
2025-07-11 09:15:09 +0900

    design : 모바일 에이아웃 용 Container, Header 컴포넌트 추가

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,23 +1,11 @@
 'use client';
 
 import Container from '@/components/layout/Container';
-import Header from '@/components/layout/Header';
-import Image from 'next/image';
 
 export default function App() {
     return (
-        <Container>
-            <Header className="justify-end">
-                <a href="#">
-                    <Image
-                        src="/settings.svg"
-                        alt="Settings"
-                        width={24}
-                        height={24}
-                    />
-                </a>
-            </Header>
-            <h1>홈페이지</h1>
+        <Container headerType="home">
+            <p>홈페이지</p>
         </Container>
     );
 }
