c4a33ca8ed7960654d8bc125008dcb2a753f7cfb
nno3onn <nno3onn@naver.com>
2025-07-13 01:23:53 +0900

    feat: 공통 버튼 컴포넌트 생성

---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react';
+import { Button } from '@/components/ui/button';
+
+interface ButtonProps {
+    type: 'primary' | 'secondary' | 'step';
+    children: ReactNode;
+}
+
+const primary = 'bg-[#3B82F6] hover:cursor-pointer hover:bg-blue-300';
+const secondary =
+    'text-[#3B82F6], bg-[#EFF6FF] hover:cursor-pointer hover:bg-white hover:bg-[#3B82F6]';
+const step = 'text-black bg-white hover:text-white hover_bg-black';
+
+export default function CButton({ type, children }: ButtonProps) {
+    const getCSS = () => {
+        switch (type) {
+            case 'primary':
+                return primary;
+            case 'secondary':
+                return secondary;
+            case 'step':
+                return step;
+            default:
+                return 'primary';
+        }
+    };
+    return <Button className={getCSS() as string}>{children}</Button>;
+}
