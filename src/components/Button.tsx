a83c9911ed76d61c81f8271d9ddc2b869815b825
nno3onn <nno3onn@naver.com>
2025-07-13 01:39:14 +0900

    fix: 공통 버튼 컴포넌트 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,8 +8,8 @@ interface ButtonProps {
 
 const primary = 'bg-[#3B82F6] hover:cursor-pointer hover:bg-blue-300';
 const secondary =
-    'text-[#3B82F6], bg-[#EFF6FF] hover:cursor-pointer hover:bg-white hover:bg-[#3B82F6]';
-const step = 'text-black bg-white hover:text-white hover_bg-black';
+    'text-[#3B82F6] bg-[#EFF6FF] hover:cursor-pointer hover:text-white hover:bg-[#3B82F6]';
+const step = 'text-black bg-white hover:text-white hover:bg-black';
 
 export default function CButton({ type, children }: ButtonProps) {
     const getCSS = () => {
@@ -21,8 +21,12 @@ export default function CButton({ type, children }: ButtonProps) {
             case 'step':
                 return step;
             default:
-                return 'primary';
+                return primary;
         }
     };
-    return <Button className={getCSS() as string}>{children}</Button>;
+    return (
+        <Button className={`${getCSS()} p-7 w-[100%] rounded-xl`}>
+            {children}
+        </Button>
+    );
 }
