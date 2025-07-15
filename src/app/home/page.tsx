5f9a521ca9f9d0aa6625fb368c8652dd8913b236
정민규 <jmg2165@naver.com>
2025-07-15 14:36:50 +0900

    chore: AI 리포트 컴포넌트 분리

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,47 +1,16 @@
 'use client';
 
 import FullContainer from '@/components/layout/FullContainer';
-import { Button } from '@/components/ui/button';
 import OrderHistory from '@/features/delivery-history/components/DeliveryHistory';
 import MonthlyReport from '@/features/report/components/MonthlyReport';
 import WeeklyReport from '@/features/report/components/WeeklyReport';
-import Image from 'next/image';
-import Link from 'next/link';
+import AIReport from './AIReport';
 
 export default function App() {
     return (
         <FullContainer headerType="home">
             <div className="space-y-8">
-                <section className="px-4">
-                    <div className="p-4 rounded-20 border-2 border-blue-200 box-border bg-white mt-4">
-                        <div className="flex justify-between">
-                            <p className="text-slate-500 text-sm font-medium">
-                                이번주 AI 요약이 도착했어요
-                            </p>
-                            <Image
-                                alt="AI 레포트"
-                                src="/smart_toy.svg"
-                                width={16}
-                                height={16}
-                            />
-                        </div>
-                        <p className="text-xl text-slate-900 font-semibold">
-                            이 주간 리포트를 보니까...
-                        </p>
-                        <p className="text-xl text-blue-500 font-semibold">
-                            배달앱 마케팅 팀이 좋아하겠네요.
-                        </p>
-                        <Button
-                            className="w-full bg-blue-50 mt-3 p-6"
-                            variant="ghost"
-                        >
-                            <Link href="#" className="text-blue-500">
-                                요약 확인하기
-                            </Link>
-                        </Button>
-                    </div>
-                </section>
-
+                <AIReport />
                 <MonthlyReport />
                 <WeeklyReport />
                 <OrderHistory pageSize={3} />
